d46e3653159064839f448257fb013bbb608b9f67
likogan <me@likogan.dev>
2023-08-01 16:51:25 -0600

    Modify slstatus layout

--- config.h
+++ config.h
@@ -7,7 +7,7 @@ const unsigned int interval = 1000;
 static const char unknown_str[] = "Not connected";
 
 /* maximum output string length */
-#define MAXLEN 4096
+#define MAXLEN 2048
 
 /*
  * function            description                     argument (example)
@@ -69,17 +69,22 @@ static const struct arg args[] = {
 	{ netspeed_rx,   " %s KiB/s  ",         "enp6s0"                                                              },
 	{ run_command,   "^c#928374^|^d^   ",    "echo NULL"                                                           },
 	// username@host
-	{ username,      "^c#fe8019^%s",        NULL                                                                  },
-	{ hostname,      "@%s  ",                NULL                                                                  },
+//	{ username,      "^c#fe8019^%s",        NULL                                                                  },
+//	{ hostname,      "@%s  ",                NULL                                                                  },
+	{ wifi_essid,    "^c#fb4934^%s   ",     "enp6s0"                                                              },
 	{ run_command,   "^c#928374^|^d^   ",    "echo NULL"                                                           },
-	// Wifi & Volume
-	{ wifi_essid,    "^c#fb4934^%s   ",     "wlan0"                                                               },
-	{ run_command,   "^c#8ec07c^%s  ",      "amixer sget Master | awk -F\"[][]\" '/%/ { print $2 }' | head -n1"   },
+	// Battery, Brightness, Volume
+//	{ battery_perc,  "^c#fe9019^%s%%  ",    "BAT0"                                                                },
+//	{ battery_remaining,"(%s)  ",            "BAT0"                                                                },
+//	{ battery_state,    "%s   ",             "BAT0"                                                                },
+
+//	{ run_command,   "^c#83a598^%s%%   ",   "echo 50"                                                             }, // ToDo: display brightness of backlight
+	{ run_command,   "^c#8ec07c^%s   ",     "amixer sget Master | awk -F\"[][]\" '/%/ { print $2 }' | head -n1"   },
 	{ run_command,   "^c#928374^|^d^   ",    "echo NULL"                                                           },
 	// CPU & RAM Usage
 	{ cpu_perc,      "^c#b8bb26^%s%%   ",   NULL                                                                  },
 	{ ram_perc,      "^c#d3869b^%s%%  ",    NULL                                                                  },
 	{ run_command,   "^c#928374^|^d^   ",    "echo NULL"                                                           },
 	// Date & time
-	{ datetime,      "^c#ebdbb2^ %s    ",   "%a %d %b %H:%M:%S"                                                   }
+	{ datetime,      "^c#ebdbb2^ %s       ","%a %d %b %H:%M:%S"                                                   }
 };
